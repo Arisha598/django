a523e60539f9800cf6652700ecce39deed551437
Arisha598 <arishachaudhary56@gmail.com>
2024-07-24 22:58:49 +0530

    TASK#4

--- cool/models.py
+++ cool/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    age=models.IntegerField()
+    email=models.EmailField()
+    address=models.TextField()
+
+    class company(models.Model):
+        name = models.CharField(max_length=50)
+        email=models.EmailField()
+    address=models.TextField()
